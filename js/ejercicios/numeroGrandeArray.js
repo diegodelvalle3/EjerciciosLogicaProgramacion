9a934e2d559a875df3db69a1adefe73d2af68757
diegodelvalle3 <diegorogito@outlook.com>
2025-04-23 14:18:27 -0600

    Ejercicio de numero mayor en un array

---
+++ js/ejercicios/numeroGrandeArray.js
@@ -0,0 +1,14 @@
+function encontrarMayor() {
+    let array = [1, 3, 5, 6, 7]
+    let numeroActual = 1
+    let numeroValidador
+
+    array.forEach((numero) => {
+        console.log(`${numero}`);
+
+    })
+
+    console.log(`El número mayor del array es: ${numeroValidador}`);
+}
+
+encontrarMayor()
