536efcc8e5d436d0053099672c9bf74d08773596
diegodelvalle3 <diegorogito@outlook.com>
2025-05-22 12:36:14 -0600

    Clases

--- js/ejercicios/clases.js
+++ js/ejercicios/clases.js
@@ -1,5 +1,5 @@
 //Expresion class
-class Perrito {
+class Animal {
     name = ''
     age = ''
 
@@ -17,11 +17,42 @@ class Perrito {
     }
 
     calcAlimentation() {
-        let totalAlimetation = ''
-        return `${name} debe comer (${age} * 3)kg!`
+        let totalAlimetation = this.age * 3
+        return `${this.name} debe comer ${totalAlimetation}kg!`
+    }
+}
+
+//Clse extiende de otra
+class Perrito extends Animal {
+    constructor(name, age) {
+        super(name, age)
     }
 }
 
 const perrito1 = new Perrito('Cooper', 7)
 
-console.log(perrito1)
+console.log(perrito1.getName())
+console.log(perrito1.getAge())
+console.log(perrito1.calcAlimentation())
+
+
+class Elefante extends Animal {
+    constructor(name, age) {
+        super(name, age)
+    }
+
+    calcAlimentation() {
+        let totalAlimetationl = this.age * 10
+        return `${this.name} debe comer ${totalAlimetationl}`
+    }
+}
+
+const elefante1 = new Elefante('Kumby', 7)
+console.log(elefante1)
+console.log(elefante1.getAge());
+console.log(elefante1.getName());
+console.log(elefante1.calcAlimentation());
+
+
+
+
